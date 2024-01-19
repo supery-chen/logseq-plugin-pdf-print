cf5a76bca98b2306efd2970857fd4583d08f4083
superychen <superychen@gmail.com>
2024-01-19 02:35:25 +0000

    fix highlight problem

--- public/index.js
+++ public/index.js
@@ -4,77 +4,27 @@ const model = {
   printPdf() {
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.text = "window.print()";
+    script.text = "window.print({printBackground: true})";
     parent.document.body.appendChild(script);
     parent.document.body.removeChild(script);
   },
 };
 
 async function main() {
-  logseq.provideStyle(`
-  -webkit-print-color-adjust:exact;
-  `);
-  logseq.provideStyle(`
-  @media print {    
-    * {
-      overflow:visible !important;
-    }
-    
-    div[class^="CodeMirror"] {
-      overflow:hidden !important;
-    }
-
-    span[class^="hide-tail"] {
-      overflow:hidden !important;
-    }
-    
-    .cm-s-light > div > textarea {
-      display: none;
-    }
-
-    .cm-s-solarized.cm-s-light {
-      background-color: #fdf6e3 !important;
-    }
-    
-    .CodeMirror-gutters {
-      display:none;
-    }
-
-    .CodeMirror-linenumber {
-      display:none;
-    }
-    .CodeMirror-sizer {
-      margin-left: 1px !important;
-      margin-bottom: 1px !important;
-      min-height: 10px !important;
-    }
-    
-    .page {
-      page-break-after:always;
-    }
-    #head {
-      display:none;
-    }
-    #left-sidebar {
-      display:none;
-    }
-    #right-sidebar {
-      display:none;
-    }
-    .references {
-      display:none;
-    }
-    .cp__sidebar-help-btn {
-      display:none;
-    }
-  }
-  `);
-  logseq.App.registerUIItem("toolbar", {
-    key: `logseq-plugin-pdf-print`,
-    template: `<a data-on-click="printPdf" title="打印pdf" class="button"><i class="ti ti-printer"></i></a>`,
-  });
-
-  console.log("#logseq-plugin-pdf-print loaded");
+  fetch('print.css')
+    .then(response => response.text())
+    .then(cssContent => {
+      // console.log(cssContent);
+      logseq.provideStyle(cssContent);
+      logseq.App.registerUIItem("toolbar", {
+        key: `logseq-plugin-pdf-print`,
+        template: `<a data-on-click="printPdf" title="PDF" class="button"><i class="ti ti-printer"></i></a>`,
+      });
+      console.log("#logseq-plugin-pdf-print loaded");
+    })
+    .catch(error => {
+      console.error('#logseq-plugin-pdf-print load error:', error);
+    });
 }
 
 logseq.ready(model, main).catch(console.error);
