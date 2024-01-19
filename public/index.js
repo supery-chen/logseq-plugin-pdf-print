09fab9b22921da31590e3e181621ca5560aa8127
superychen <superychen@gmail.com>
2024-01-19 03:22:40 +0000

    optimize codes

--- public/index.js
+++ public/index.js
@@ -1,10 +1,30 @@
 import "@logseq/libs";
 
+const scripts = `
+function hidePrint(elements, excludeIds) {
+  Array.from(elements)
+      .forEach(function (element) {
+          var childId = element.id;
+          if (!excludeIds.includes(childId) && !element.classList.contains('print-hide')) {
+              element.classList.add('print-hide')
+          }
+      });
+}
+
+hidePrint(document.querySelector('main').children, ['app-container']);
+hidePrint(document.getElementById('app-container').children, ['left-container']);
+hidePrint(document.getElementById('left-container').children, ['main-container']);
+hidePrint(document.getElementById('main-container').children, ['main-content-container']);
+
+// window.print({ printBackground: true });
+window.print();
+`
+
 const model = {
   printPdf() {
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.text = "window.print({printBackground: true})";
+    script.text = scripts;
     parent.document.body.appendChild(script);
     parent.document.body.removeChild(script);
   },
